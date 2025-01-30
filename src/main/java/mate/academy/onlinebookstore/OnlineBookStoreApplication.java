217c6320ff0bfcb66a7be4016b3b464a0b2961c4
Panchenko Dmytro <panchenko0795@gmail.com>
2025-01-30 23:49:40 +0200

    Added Book entity, BookRepository and BookService

--- src/main/java/mate/academy/onlinebookstore/OnlineBookStoreApplication.java
+++ src/main/java/mate/academy/onlinebookstore/OnlineBookStoreApplication.java
@@ -1,11 +1,20 @@
 package mate.academy.onlinebookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OnlineBookStoreApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            System.out.println("Application has started!");
+        };
     }
 }

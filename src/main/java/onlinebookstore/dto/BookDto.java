cb20f97ee1e30cb07173af192e5b5c5ad31c9001
Panchenko Dmytro <panchenko0795@gmail.com>
2025-08-13 22:04:36 +0300

    Improved solution:  - added server.servlet.context-path=/api to application.properties;  - substituted annotations in DTOs classes to @Data;  - BookServiceImpl class:    - substituted constructor to @RequiredArgsConstructor annotation;    - removed unnecessary null check;    - substituted '.collect(Collectors.toList())' to '.toList();' in getAll() method;    - renamed variable from 'saved' to 'book' in save() method.

--- src/main/java/onlinebookstore/dto/BookDto.java
+++ src/main/java/onlinebookstore/dto/BookDto.java
@@ -1,17 +1,9 @@
 package onlinebookstore.dto;
 
 import java.math.BigDecimal;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.Data;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
+@Data
 public class BookDto {
     private Long id;
     private String title;
--- src/main/java/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/onlinebookstore/service/impl/BookServiceImpl.java
@@ -1,7 +1,7 @@
 package onlinebookstore.service.impl;
 
 import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import onlinebookstore.dto.BookDto;
 import onlinebookstore.dto.CreateBookRequestDto;
 import onlinebookstore.exception.EntityNotFoundException;
@@ -9,41 +9,29 @@ import onlinebookstore.mapper.BookMapper;
 import onlinebookstore.model.Book;
 import onlinebookstore.repository.BookRepository;
 import onlinebookstore.service.BookService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
     private final BookRepository bookRepository;
     private final BookMapper bookMapper;
 
-    @Autowired
-    public BookServiceImpl(BookRepository bookRepository, BookMapper bookMapper) {
-        this.bookRepository = bookRepository;
-        this.bookMapper = bookMapper;
-    }
-
     @Override
     public BookDto save(CreateBookRequestDto bookRequestDto) {
-        if (bookRequestDto == null) {
-            throw new RuntimeException("bookRequestDto can't be null");
-        }
-        Book saved = bookRepository.save(bookMapper.toModel(bookRequestDto));
-        return bookMapper.toDto(saved);
+        Book book = bookRepository.save(bookMapper.toModel(bookRequestDto));
+        return bookMapper.toDto(book);
     }
 
     @Override
     public List<BookDto> findAll() {
         return bookRepository.findAll().stream()
                 .map(bookMapper::toDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
     public BookDto findById(Long id) {
-        if (id == null) {
-            throw new RuntimeException("id can't be null");
-        }
         Book book = bookRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Book not found with id: " + id));
         return bookMapper.toDto(book);
